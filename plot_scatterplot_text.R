b426d63b08df325ca8ab98f55f286d3c9d07495e
Erik Ländström <erik.landstrom@hotmail.se>
2018-12-20 12:16:42 +0100

    Plots a scatterplot using character strings instead of points

---
+++ plot_scatterplot_text.R
@@ -0,0 +1,64 @@
+### plot_scatterplot_text
+### Author: Erik Ländström
+### Date: 181220
+
+
+# Description -------------------------------------------------------------
+
+# Plots a scatterplot with character strings instead of dots,
+# with geom smoot being optional.
+
+
+# Arguments ---------------------------------------------------------------
+
+# data = tibble,
+# column1 = column1
+# column2 = column2
+# text = character, strings to plot instead of dots
+# ylabel = character, y label
+# xlabel = character, x label
+# title_string = option, plot title
+# subtitle_string = option, plot subtitle
+# caption title = optional, plot caption
+# smooth = logical, defaults to FALSE. If TRUE plots a linear smoothing curve.
+
+
+# Function ----------------------------------------------------------------
+
+plot_scatterplot_text <- function(data, column1, column2, text, y_label, x_label, 
+                             title_string = "Scatterplot", 
+                             subtitle_string = NULL, caption_string = NULL, 
+                             smooth = FALSE) {
+  # Library
+  library(tidyverse)
+  
+  # Quote column names
+  column1 <- enquo(column1)
+  column2 <- enquo(column2)
+  text <- enquo(text)
+  
+  # Scatter plot
+  plot <- data %>%
+    ggplot(aes(!!column1, !!column2)) +
+    geom_text(aes(label = !!text)) +
+    xlab(x_label) +
+    ylab(y_label) +
+    labs(
+      title = title_string,
+      subtitle = subtitle_string,
+      caption = caption_string
+    ) +
+    theme(
+      panel.background = element_blank(),
+      axis.line = element_line(color = "black")
+    )
+  
+  # Plots smoothing curve if smooth = TRUE
+  if (smooth == TRUE) {
+    plot +
+      geom_smooth(method = "lm", color = "red", se = FALSE)
+  }
+  else {
+    plot
+  }
+} 
