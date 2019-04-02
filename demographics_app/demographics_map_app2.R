8cdf33c833e15559b32c24549a9c440bff210c8e
Erik Ländström <erik.landstrom@hotmail.se>
2019-04-02 11:56:55 +0200

    Created my first Shiny app, simple plots of US demographic distributions.
    Want to update with Alaska and Hawaii, better color gradient and more interactivity

---
+++ demographics_app/demographics_map_app2.R
@@ -0,0 +1,67 @@
+# Load libraries ----------------------------------------------------------
+
+library(shiny)
+library(tidyverse)
+library(maps)
+library(ggplot2)
+
+# Load data ---------------------------------------------------------------
+
+county_demo_data <- read_tsv("data/2015_demographics_census_county_data.txt")
+
+ui <- fluidPage(
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+               information from the 2015 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", 
+                              "Percent Black",
+                              "Percent Hispanic", 
+                              "Percent Asian",
+                              "Percent Native American",
+                              "Percent Pacific islanders"),
+                  selected = "Percent Hispanic")
+      ),
+    
+    mainPanel(
+      plotOutput("map")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$map <- renderPlot({
+    ethnicity <- switch(input$var,
+                   "Percent White"             = county_demo_data$White, 
+                   "Percent Black"             = county_demo_data$Black,
+                   "Percent Hispanic"          = county_demo_data$Hispanic, 
+                   "Percent Asian"             = county_demo_data$Asian,
+                   "Percent Native American"   = county_demo_data$Native,
+                   "Percent Pacific islanders" = county_demo_data$Pacific)
+    
+    ggplot(data = county_demo_data) + 
+      geom_polygon(aes(x = long, y = lat, group = group, fill = ethnicity), color = "black") + 
+      scale_fill_gradientn(colours = rev(rainbow(6)),
+                           breaks  = c(0, 25, 50, 75, 100)) +
+      # Removes the axes, lines, panels etc
+      theme(
+        axis.text = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        panel.background = element_blank(),
+        axis.title = element_blank()
+      )
+    
+  })
+  
+}
+
+shinyApp(ui, server)
